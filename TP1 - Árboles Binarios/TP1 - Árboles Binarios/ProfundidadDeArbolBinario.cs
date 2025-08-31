0935386e2b4434432f37c775a11a1bf73eb63ddc
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-08-31 13:11:47 -0300

    Add files via upload

---
+++ TP1 - Árboles Binarios/TP1 - Árboles Binarios/ProfundidadDeArbolBinario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace tp1
+{
+    public class ProfundidadDeArbolBinario
+    {
+        private ArbolBinario<int> arbol;
+
+        public ProfundidadDeArbolBinario(ArbolBinario<int> arbol)
+        {
+            this.arbol = arbol;
+        }
+
+        public int sumaElementosProfundidad(int p)
+        {
+            return sumar(this.arbol, p, 0);
+        }
+
+        private int sumar(ArbolBinario<int> nodo, int nivelBuscado, int nivelActual)
+        {
+            if (nodo == null) return 0;
+
+            if (nivelActual == nivelBuscado)
+                return nodo.getDatoRaiz();
+
+            int sumaIzq = sumar(nodo.getHijoIzquierdo(), nivelBuscado, nivelActual + 1);
+            int sumaDer = sumar(nodo.getHijoDerecho(), nivelBuscado, nivelActual + 1);
+
+            return sumaIzq + sumaDer;
+        }
+    }
+}
---
+++ TP1 - Árboles Binarios/TP1 - Árboles Binarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tp1;
+
+class Program
+{
+    static void Main()
+    {
+        ArbolBinario<int> raiz = new ArbolBinario<int>(0);
+        raiz.agregarHijoIzquierdo(new ArbolBinario<int>(1));
+        raiz.agregarHijoDerecho(new ArbolBinario<int>(4));
+        raiz.getHijoIzquierdo().agregarHijoIzquierdo(new ArbolBinario<int>(2));
+        raiz.getHijoIzquierdo().agregarHijoDerecho(new ArbolBinario<int>(3));
+        raiz.getHijoDerecho().agregarHijoDerecho(new ArbolBinario<int>(5));
+
+        ProfundidadDeArbolBinario prof = new ProfundidadDeArbolBinario(raiz);
+
+        int suma = prof.sumaElementosProfundidad(2);
+        Console.WriteLine("suma de los nodos en profundidad 2: " + suma);
+
+        Console.ReadKey();
+    }
+}
